2a13159d25bfce963b7d4585884b04b6bab27e73
Silvio Quadri <silvioq@gmail.com>
2017-05-02 17:59:16 -0300

    Test for duplicated join

--- Tests/BuilderTest.php
+++ Tests/BuilderTest.php
@@ -3,8 +3,9 @@
 namespace  Silvioq\ReportBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
-
+use Doctrine\ORM\EntityManager;
 use Silvioq\ReportBundle\Datatable\Builder;
+use Silvioq\ReportBundle\Datatable\BuilderException;
 
 class  BuilderTest  extends  TestCase
 {
@@ -47,6 +48,28 @@ class  BuilderTest  extends  TestCase
         $dt = new Builder( $emMock, [ "draw" => 'me' ] );
         $this->assertEquals( 'me', $dt->getDraw() );
     }
-    
+
+    /**
+     * @covers Builder::join
+     */
+    public function testTrhowOnAlreadyJoined()
+    {
+        $emMock = $this
+            ->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dt = new Builder( $emMock, [ "search" => [ "value" => "x" ] ] );
+        $dt
+            ->add( 'field1' )
+            ->add( 'field2' )
+            ->join( 'field3', 'd' )
+            ->from( 'Test:Table', 'a' );
+
+        $dt->join( 'field4', 'c' );
+        $this->expectException( BuilderException::class );
+        $dt->join( 'field4', 'c' );
+
+    }
 
 }
