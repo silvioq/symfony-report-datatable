629c1be32e4054d4c9403b23ddc1d4221f3ae0aa
silvioq <silvioq@gmail.com>
2017-05-02 00:17:35 -0300

    Testeo de limit y offset

--- Tests/ResultTest.php
+++ Tests/ResultTest.php
@@ -60,9 +60,6 @@ class  ResultTest  extends  TestCase
         
     }
     
-
-
-
     /**
      * @covers getQuery
      */
@@ -98,6 +95,65 @@ class  ResultTest  extends  TestCase
             
         $dt = new Builder( $emMock, 
             [ "search" => [ "value" => "x" ] ] );
+        $dt
+            ->add( 'field1' )
+            ->add( 'field2' )
+            ->from( 'Test:Table', 'a' )
+            ;
+
+        $this->assertInstanceOf( \Doctrine\ORM\AbstractQuery::class, $dt->getQuery() );
+        
+    }
+
+
+
+    /**
+     * @covers getQuery
+     */
+    public  function  testGetQueryWithLimit()
+    {
+        $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
+               array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
+        $repoMock = $this->createMock( '\Doctrine\ORM\EntityRepository', ["createQueryBuilder"], array(), '', false );
+        $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery", "andWhere", 'setMaxResults', 'setFirstResult'], array(), '', false );
+        $queryMock = $this->createMock( '\Doctrine\ORM\AbstractQuery', ['getResult'], array(), '', false );
+        
+        $emMock->expects($this->once())
+            ->method('getRepository')
+            ->with($this->equalTo('Test:Table'))
+            ->will($this->returnValue($repoMock))
+            ;
+
+        $repoMock->expects($this->once())
+            ->method("createQueryBuilder")
+            ->with($this->equalTo('a'))
+            ->will($this->returnValue( $qbMock ) )
+            ;
+            
+        $qbMock->expects($this->once())
+            ->method('select')
+            ->will($this->returnSelf() )
+            ;
+            
+        $qbMock->expects($this->once())
+            ->method('setMaxResults')
+            ->with($this->equalTo(20))
+            ->will($this->returnSelf())
+            ;
+
+        $qbMock->expects($this->once())
+            ->method('setFirstResult')
+            ->with($this->equalTo(10))
+            ->will($this->returnSelf())
+            ;
+
+        $qbMock->expects($this->once())
+            ->method('getQuery')
+            ->will( $this->returnValue($queryMock) )
+            ;
+        
+        $dt = new Builder( $emMock, 
+            [ "search" => [ "value" => '' ], "start" => 10, "length" => 20 ] );
         $dt
             ->add( 'field1' )
             ->add( 'field2' )
