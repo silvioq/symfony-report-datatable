e8837bce7fbb9a03671dde79e4ae9df283600bc5
silvioq <silvioq@gmail.com>
2017-05-01 23:53:40 -0300

    Mejoro los tests

--- Tests/WheresTest.php
+++ Tests/WheresTest.php
@@ -6,21 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 use Silvioq\ReportBundle\Datatable\Builder;
 
-class  ResultTest  extends  TestCase
+class  WheresTest  extends  TestCase
 {
-
     /**
-     * @covers Builder::add
-     * @covers Builder::from
+     * @covers Builder::where
+     * @covers Builder::whereOnFiltered
+     * @covers Builder::addWheresToCB
+     * @dataProvider whereFunctions
      */
-    public  function  testQueryResult()
-    {
+    function testAndWhereOnQueryBuilder($functionName)
+    {     
         $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
                array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
         $repoMock = $this->createMock( '\Doctrine\ORM\EntityRepository', ["createQueryBuilder"], array(), '', false );
         $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery", "andWhere"], array(), '', false );
         $queryMock = $this->createMock( '\Doctrine\ORM\AbstractQuery', ['getResult'], array(), '', false );
-        
+
         $emMock->expects($this->once())
             ->method('getRepository')
             ->with($this->equalTo('Test:Table'))
@@ -42,6 +43,11 @@ class  ResultTest  extends  TestCase
             ->method('getQuery')
             ->will( $this->returnValue($queryMock) )
             ;
+        
+        $qbMock->expects($this->once())
+            ->method('andWhere')
+            ->with($this->equalTo( 'a.field1 = 1') )
+            ;
             
         $queryMock->expects($this->once())
             ->method('getResult')
@@ -54,19 +60,21 @@ class  ResultTest  extends  TestCase
             ->add( 'field1' )
             ->add( 'field2' )
             ->from( 'Test:Table', 'a' )
+            ->$functionName( 'a.field1 = 1')
             ;
 
         $this->assertEquals( $dt->getResult(), [] );
-        
     }
-    
+
 
 
     /**
      * @covers Builder::where
+     * @covers Builder::whereOnFiltered
      * @covers Builder::addWheresToCB
+     * @dataProvider whereFunctions
      */
-    function testAndWhereWithCallable()
+    function testAndWhereWithCallable($functionName)
     {     
         $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
                array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
@@ -116,7 +124,7 @@ class  ResultTest  extends  TestCase
             ->add( 'field1' )
             ->add( 'field2' )
             ->from( 'Test:Table', 'a' )
-            ->where( function($qb) use( &$called, $_self ){
+            ->$functionName( function($qb) use( &$called, $_self ){
                 $called ++;
                 $_self->assertInstanceOf( \Doctrine\ORM\QueryBuilder::class, $qb );
             } )
@@ -125,19 +133,20 @@ class  ResultTest  extends  TestCase
         $this->assertEquals( $dt->getResult(), [] );
         $this->assertEquals( 1, $called );
     }
-
+    
 
     /**
-     * @covers getQuery
+     * @covers Builder::where
+     * @covers Builder::whereOnFiltered
      */
-    public  function  testGetQuery()
-    {
+    function testAndWhereCalledFromCount()
+    {     
         $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
                array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
         $repoMock = $this->createMock( '\Doctrine\ORM\EntityRepository', ["createQueryBuilder"], array(), '', false );
-        $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery", "andWhere"], array(), '', false );
+        $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery", "andWhere", 'setMaxResults'], array(), '', false );
         $queryMock = $this->createMock( '\Doctrine\ORM\AbstractQuery', ['getResult'], array(), '', false );
-        
+
         $emMock->expects($this->once())
             ->method('getRepository')
             ->with($this->equalTo('Test:Table'))
@@ -154,10 +163,26 @@ class  ResultTest  extends  TestCase
             ->method('select')
             ->will($this->returnSelf() )
             ;
+
+        $qbMock->expects($this->once())
+            ->method('setMaxResults')
+            ->with($this->equalTo( '1') )
+            ->will($this->returnSelf())
+            ;
             
         $qbMock->expects($this->once())
             ->method('getQuery')
             ->will( $this->returnValue($queryMock) )
+            ;
+
+        $qbMock->expects($this->once())
+            ->method('andWhere')
+            ->with($this->equalTo( 'a.field1 = 1') )
+            ;
+
+        $queryMock->expects($this->once())
+            ->method('getResult')
+            ->will( $this->returnValue([[0,3]]))
             ;
             
         $dt = new Builder( $emMock, 
@@ -166,10 +191,90 @@ class  ResultTest  extends  TestCase
             ->add( 'field1' )
             ->add( 'field2' )
             ->from( 'Test:Table', 'a' )
+            ->where( 'a.field1 = 1')
+            ->whereOnFiltered( 'a.field2 = 2' )
             ;
 
-        $this->assertInstanceOf( \Doctrine\ORM\AbstractQuery::class, $dt->getQuery() );
-        
+        $this->assertEquals( 3, $dt->getCount() );
     }
+
+    
+
+    /**
+     * @covers Builder::where
+     * @covers Builder::whereOnFiltered
+     */
+    function testAndWhereCalledFromFilteredCount()
+    {     
+        $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
+               array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
+        $repoMock = $this->createMock( '\Doctrine\ORM\EntityRepository', ["createQueryBuilder"], array(), '', false );
+        $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery", "andWhere", 'setMaxResults'], array(), '', false );
+        $queryMock = $this->createMock( '\Doctrine\ORM\AbstractQuery', ['getResult'], array(), '', false );
+
+        $emMock->expects($this->once())
+            ->method('getRepository')
+            ->with($this->equalTo('Test:Table'))
+            ->will($this->returnValue($repoMock))
+            ;
+
+        $repoMock->expects($this->once())
+            ->method("createQueryBuilder")
+            ->with($this->equalTo('a'))
+            ->will($this->returnValue( $qbMock ) )
+            ;
+            
+        $qbMock->expects($this->once())
+            ->method('select')
+            ->will($this->returnSelf() )
+            ;
+
+        $qbMock->expects($this->exactly(0))
+            ->method('setMaxResults')
+            ->with($this->equalTo( '1') )
+            ->will($this->returnSelf())
+            ;
+            
+        $qbMock->expects($this->once())
+            ->method('getQuery')
+            ->will( $this->returnValue($queryMock) )
+            ;
+
+        $qbMock->expects($this->at(1))
+            ->method('andWhere')
+            ->with($this->equalTo( 'a.field2 = 2') )
+            ;
+        $qbMock->expects($this->at(2))
+            ->method('andWhere')
+            ->with($this->equalTo( 'a.field1 = 1') )
+            ;
+
+        $queryMock->expects($this->once())
+            ->method('getResult')
+            ->will( $this->returnValue([[0,3]]))
+            ;
+            
+        $dt = new Builder( $emMock, 
+            [ "search" => [ "value" => "x" ] ] );
+        $dt
+            ->add( 'field1' )
+            ->add( 'field2' )
+            ->from( 'Test:Table', 'a' )
+            ->where( 'a.field1 = 1')
+            ->whereOnFiltered( 'a.field2 = 2' )
+            ;
+
+        $this->assertEquals( 3, $dt->getFilteredCount() );
+    }
+
+
+
+
+    public function whereFunctions()
+    {
+        return[ ["where"], ["whereOnFiltered"] ];
+    }
+
+
 
 }
