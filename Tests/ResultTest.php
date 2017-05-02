c7c1031fb6604263da0b49d3a97b611e9f961d94
silvioq <silvioq@gmail.com>
2017-05-02 02:15:57 -0300

    test de addHidden

--- Tests/ResultTest.php
+++ Tests/ResultTest.php
@@ -3,8 +3,8 @@
 namespace  Silvioq\ReportBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
-
 use Silvioq\ReportBundle\Datatable\Builder;
+use Doctrine\DBAL\Types\Type as ORMType;
 
 class  ResultTest  extends  TestCase
 {
@@ -187,6 +187,85 @@ class  ResultTest  extends  TestCase
             [ false, 50 ],
             [ false, false ],
            ] ;
+    }
+
+
+
+    /**
+     * @cover Builder::addHidden
+     */
+    public  function  testGetResultWithHiddenColumn()
+    {
+        $emMock  = $this->createMock('\Doctrine\ORM\EntityManager',
+               array('getRepository', 'getClassMetadata', 'persist', 'flush'), array(), '', false);
+        $repoMock = $this->createMock( '\Doctrine\ORM\EntityRepository', ["createQueryBuilder"], array(), '', false );
+        $qbMock = $this->createMock( '\Doctrine\ORM\QueryBuilder', ["select", "getQuery"], array(), '', false );
+        $queryMock = $this->createMock( '\Doctrine\ORM\AbstractQuery', ['getResult'], array(), '', false );
+        $metadataMock = $this->createMock( '\Doctrine\ORM\Mapping\ClassMetadataInfo', ['getFieldNames','getTypeOfField'], array(), '', false );
+
+        $result = [
+            [ 'field1' => 1, 'field2' => 1, 'field3' => 2, ],
+            [ 'field1' => 2, 'field2' => 2, 'field3' => 4, ],
+            [ 'field1' => 3, 'field2' => 3, 'field3' => 5, ]
+        ];
+        
+        $emMock->expects($this->once())
+            ->method('getRepository')
+            ->with($this->equalTo('Test:Table'))
+            ->will($this->returnValue($repoMock))
+            ;
+
+        $emMock->expects($this->once())
+            ->method('getClassMetadata')
+            ->with($this->equalTo('Test:Table'))
+            ->will($this->returnValue($metadataMock))
+            ;
+        
+        $repoMock->expects($this->once())
+            ->method("createQueryBuilder")
+            ->with($this->equalTo('a'))
+            ->will($this->returnValue( $qbMock ) )
+            ;
+
+        $qbMock->expects($this->once())
+            ->method('select')
+            ->will($this->returnSelf() )
+            ;
+
+        $qbMock->expects($this->once())
+            ->method('getQuery')
+            ->will( $this->returnValue($queryMock) )
+            ;
+
+        $queryMock->expects($this->once())
+            ->method('getResult')
+            ->will( $this->returnValue($result))
+            ;
+
+        $metadataMock->expects($this->atLeastOnce())
+            ->method('getTypeOfField')
+            ->will( $this->returnValue(ORMType::INTEGER))
+            ;
+
+        $metadataMock->expects($this->once())
+            ->method('getFieldNames')
+            ->will( $this->returnValue(['field1','field2','field3']))
+            ;
+
+        $dt = new Builder( $emMock, [ "search" => [ "value" => "x" ] ] );
+        $dt
+            ->add( 'field1' )
+            ->add( 'field2' )
+            ->addHidden( 'field3' )
+            ->from( 'Test:Table', 'a' )
+            ;
+
+        $this->assertEquals( $dt->getArray(), [
+                [ 1, 1 ],
+                [ 2, 2 ],
+                [ 3, 3 ]
+            ] );
+
     }
 
 }
