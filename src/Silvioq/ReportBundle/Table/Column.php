7d0d44cd994ce8d9315b71d32308f6422cc12da3
Silvio <silvioq@gmail.com>
2017-12-17 10:58:20 -0300

    Type hint

--- src/Silvioq/ReportBundle/Table/Column.php
+++ src/Silvioq/ReportBundle/Table/Column.php
@@ -7,15 +7,26 @@ namespace   Silvioq\ReportBundle\Table;
  */
 class Column
 {
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var string
+     */
     private $getter;
+
+    /**
+     * @var string
+     */
     private $label;
-    
-    public function __construct( $name, $label = null, $getter = null )
+
+    public function __construct($name, $label = null, $getter = null )
     {
         if( !is_string( $name ) )
             throw new \InvalidArgumentException( 'Name must be string' );
-        
+
         if( null === $label )
             $label = static::humanize( $name );
         else if( !is_string( $label ) )
@@ -30,26 +41,35 @@ class Column
         $this->label = $label;
         $this->getter = $getter;
     }
-    
-    public function getName()
+
+    /**
+     * @return string
+     */
+    public function getName():string
     {
         return $this->name;
     }
-    
-    public function getLabel()
+
+    /**
+     * @return string
+     */
+    public function getLabel():string
     {
         return $this->label;
     }
-    
+
+    /**
+     * @return callable|string
+     */
     public function getGetter()
     {
         return $this->getter;
     }
-    
+
     /**
      * @see https://github.com/symfony/symfony/blob/master/src/Symfony/Component/Form/FormRenderer.php#L306
      */
-    static public function humanize($text)
+    static public function humanize($text):string
     {
         return ucfirst(trim(strtolower(preg_replace(array('/([A-Z])/', '/[_\s]+/'), array('_$1', ' '), $text))));
     }
