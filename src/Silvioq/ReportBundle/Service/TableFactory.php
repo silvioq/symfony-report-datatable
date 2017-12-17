ef101cdcdf62f9e56fec56281a459786dc74a829
Silvio <silvioq@gmail.com>
2017-12-17 10:40:46 -0300

    Deprecation warning

---
+++ src/Silvioq/ReportBundle/Service/TableFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Silvioq\ReportBundle\Service;
+
+@trigger_error('Class Silvioq\ReportBundle\Service\TableFactory is deprecated. Use \Silvioq\ReportBundle\Table\TableFactory instead');
+
+class TableFactory extends \Silvioq\ReportBundle\Table\TableFactory
+{
+}
